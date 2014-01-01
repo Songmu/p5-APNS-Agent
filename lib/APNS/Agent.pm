0716ae15f25b15319c22fa30ffa64f43ebbf4d0a
Songmu <y.songmu@gmail.com>
2014-01-01 20:18:27 +0900

    initial implement

---
+++ lib/APNS/Agent.pm
@@ -0,0 +1,179 @@
+package APNS::Agent;
+use feature ':5.10';
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+use AnyEvent::APNS;
+use Cache::LRU;
+use Encode qw/decode_utf8/;
+use Log::Minimal;
+use JSON::XS;
+use Plack::Request;
+
+use Class::Accessor::Lite::Lazy 0.03 (
+    new => 1,
+    ro => [qw/
+        certificate
+        private_key
+        sandbox
+        reconnect_interval
+        debug_port
+    /],
+    ro_lazy => {
+        on_error_response   => sub { sub { warn @_ } },
+        disconnect_interval => sub { 60 },
+        _sent_token         => sub { Cache::LRU->new(size => 10000) },
+        _queue              => sub { [] },
+    },
+    rw => [qw/_apns _last_connected_at _last_sent_at/],
+);
+
+sub to_app {
+    my $self = shift;
+
+    sub {
+        my $env = shift;
+        my $req = Plack::Request->new($env);
+
+        return [404, [], ['NOT FOUND']] unless $req->path_info =~ m!\A/?\z!ms;
+
+        if ($req->method eq 'POST') {
+            my $token = $req->param('token') or return [400, [], ['Bad Request']];
+
+            my $payload;
+            if (my $payload_json = $req->param('payload') ) {
+                state $json_driver = JSON::XS->new->utf8;
+                local $@;
+                my $payload = eval { $json_driver->decode($payload_json) };
+                return [400, [], ['BAD REQUEST']] if $@;
+            }
+            elsif (my $alert = $req->param('alert')) {
+                $payload = +{
+                    alert => decode_utf8($alert),
+                };
+            }
+            return [400, [], ['BAD REQUEST']] unless $payload;
+
+            if ($self->_apns && $self->_apns->connected) {
+                $self->_send($token, $payload);
+                infof "[server] send notify complete %s", $token;
+            }
+            else {
+                infof "[apns] push queue";
+                push @{$self->_gueue}, [$token, $payload];
+                $self->_connect_to_apns;
+            }
+            return [200, [], ['Accepted']];
+        }
+
+        return [405, [], ['Method Not Allowed']];
+    };
+}
+
+sub _connect_to_apns {
+    my $self = shift;
+
+    return if $self->_apns;
+
+    $self->_apns(AnyEvent::APNS->new(
+        certificate => $self->certificate,
+        private_key => $self->private_key,
+        sandbox     => $self->sandbox,
+        on_error    => sub {
+            my ($handle, $fatal, $message) = @_;
+
+            my $t; $t = AnyEvent->timer(
+                after    => 0,
+                interval => 10,
+                cb       => sub {
+                    undef $t;
+                    infof "[apns] reconnect";
+                    $self->_last_connected_at(time);
+                    $self->_apns->connect;
+                },
+            );
+
+            infof "[apns] error fatal: $fatal message: $message";
+        },
+        on_connect  => sub {
+            infof "[apns] on_connect";
+            $self->_last_connected_at(time);
+
+            if (@{$self->_gueue}) {
+                while (my $q = shift @{$self->_gueue}) {
+                    $self->_send(@$q);
+                    infof "[apns] send from queue ".$q->[0];
+                }
+            }
+        },
+        on_error_response => sub {
+            my ($identifier, $state) = @_;
+            my $token = $self->_sent_token->get($identifier) || undef;
+            $self->on_error_response->($token, @_);
+        },
+    ));
+
+    if ($self->debug_apns_port) {
+        $self->_apns->debug_port($self->debug_apns_port);
+    }
+
+    $self->_apns->connect;
+
+    if (my $interval = $self->disconnect_interval) {
+        my $t; $t = AnyEvent->timer(
+            after    => $interval,
+            interval => $interval,
+            cb       => sub {
+                if ($self->_apns && (time - ($self->_last_sent_at || 0) > $interval)) {
+                    delete $self->{apns};
+                    infof "[apns] close apns";
+                    undef $t;
+                }
+            },
+        );
+    }
+}
+
+sub _send {
+    my ($self, $token, $payload) = @_;
+
+    my $identifier = $self->_apns->send(pack("H*", $token) => {
+        aps => $payload,
+    });
+    $self->_sent_token->set($identifier, $token);
+    $self->_last_sent_at(time);
+    $identifier;
+}
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+APNS::Agent - It's new $module
+
+=head1 SYNOPSIS
+
+    use APNS::Agent;
+
+=head1 DESCRIPTION
+
+APNS::Agent is ...
+
+=head1 LICENSE
+
+Copyright (C) Songmu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Songmu E<lt>y.songmu@gmail.comE<gt>
+
+=cut
+
